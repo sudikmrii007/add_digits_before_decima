897fe4b82e963a65de0a14670a0dd3e1f25b4c80
SUDIPTA KUMARI <44607575+sudikmrii007@users.noreply.github.com>
2019-07-27 23:51:21 +0530

    Update before_dec_add.c

--- before_dec_add.c
+++ before_dec_add.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
-int numAdd(float);
+int numAdd(float);     // declaration of user defined function
 void main()
 {
 	int added;
         float num;
         printf("\nenter any float number");
-        scanf("%f",&num);
+        scanf("%f",&num);              
         added=numAdd(num);
         printf("\naddition of the numbers before the decimal %d\n",added);
 }
 int numAdd(float n)
 {
 	int n1,rem,sum=0;
-	n1=n;
+	n1=n;              // storing float value in int data type will only accept the value before decimal
 	while(n1!=0)
 	{
 		rem=n1%10;
